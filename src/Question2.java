e86ad5ac000abbf5184ca2c3cdb6a29d63c27324
Aditya Mahajan <adityamahajan7287@gmail.com>
2026-01-07 13:02:29 +0530

    Initial commit Question3

---
+++ src/Question2.java
@@ -0,0 +1,11 @@
+public class Question2 {
+
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        int result = square(5);
+        System.out.println(result); // 25
+    }
+}
